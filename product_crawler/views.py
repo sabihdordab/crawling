b4e44445006813ddf8958af2db55563bfe579f59
sabih <sabihd0rd4b@gmail.com>
2024-12-30 00:08:46 +0330

    scraper products view

--- product_crawler/views.py
+++ product_crawler/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ScraperSelectionForm
+from .scrapers import DivarScraper  
+from . models import Product
 
-# Create your views here.
+def scraper_view(request):
+    if request.method == 'POST':
+        form = ScraperSelectionForm(request.POST)
+        if form.is_valid():
+            scraper_choice = form.cleaned_data['scraper']
+            if scraper_choice == 'divar':
+                divar_scraper = DivarScraper()
+                divar_scraper.run()
+                return redirect('scraper_view')
+    else:
+        form = ScraperSelectionForm()
+        products = Product.objects.all()  
+    return render(request, 'product_list.html', {'form': form, 'products': products})
